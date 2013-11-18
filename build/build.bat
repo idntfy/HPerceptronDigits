838820dd363310d465e89b3ec0faf986c5494ad9
idntfy <idntfy@gmail.com>
2013-11-18 23:58:15 +0400

    add .gitignore and fix buildscript

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-set BIN_PATH=../bin
+set BIN_PATH=../bin/windows
 set SRC_PATH=../src
 
 ghc -o "%BIN_PATH%/HPerceptronDigits" -odir "%BIN_PATH%" -hidir "%BIN_PATH%" -i"%SRC_PATH%" "%SRC_PATH%/main.hs"
