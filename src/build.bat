29097bce53490647ed1e789a0e1b5db35f8578f2
idntfy <idntfy@gmail.com>
2013-11-18 23:38:33 +0400

    Cleaning

--- src/build.bat
+++
@@ -1,3 +0,0 @@
-set BIN_PATH="../bin/" 
-ghc -o HPerceptronDigits "main.hs"
-pause
--- src/main.hs
+++ src/main.hs
@@ -17,10 +17,7 @@ main = do
     let neurons = 10
     let inputs = 64 * 64
 
-    putStrLn "Traininig perceptron..."
     perceptron <- createTrainedPerceptron neurons inputs 25 path
-    putStrLn "Perceptron trained."
-
     forever $ do 
         putStrLn "Input name of file to recognize: "
         fileName <- getLine
